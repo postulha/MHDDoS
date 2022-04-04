e4231c83ffb5718922916ce5292cc2abf3c10f28
Alex Postulha <a.postulha@paser.io>
2022-04-04 11:36:41 +0300

    Remove unused code

--- start.py
+++ start.py
@@ -196,18 +196,6 @@ def getMyIPAddress():
     return getMyIPAddress()
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    RESET = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 def exit(*message):
     if message:
         logger.error(message)
